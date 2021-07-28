860cd27e9755a4af0e2d22c795cfbbc84a8a13a8
Kevin Matt <he_yuheng@163.com>
2021-07-28 22:00:27 +0800

    Revert "增加postObjects测试"
    This reverts commit 5a239473d9a6b185e15ad38ffaadc03ca732e32b.

--- src/whosbugAssigns/web_server_util_test.go
+++ src/whosbugAssigns/web_server_util_test.go
@@ -50,30 +50,3 @@ func Test_getLatestRelease(t *testing.T) {
 		})
 	}
 }
-
-var object1 = ObjectInfoType{"1","2","3","4","5","6","7","8"}
-var object2 = ObjectInfoType{"9","10","11","12","13","14","15","16"}
-
-
-func Test_postObjects(t *testing.T) {
-	type args struct {
-		projectId      string
-		releaseVersion string
-		commitHash     string
-		objects        []ObjectInfoType
-	}
-	tests := []struct {
-		name    string
-		args    args
-		wantErr bool
-	}{
-		{"test1", args{"whosbug_test_1", "1.0.0", "0eaa40f", []ObjectInfoType{object1, object2}}, false},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if err := postObjects(tt.args.projectId, tt.args.releaseVersion, tt.args.commitHash, tt.args.objects); (err != nil) != tt.wantErr {
-				t.Errorf("postObjects() error = %v, wantErr %v", err, tt.wantErr)
-			}
-		})
-	}
-}
